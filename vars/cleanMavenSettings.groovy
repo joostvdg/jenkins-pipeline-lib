8d5b3a34e2daf7059277276bbaea139edacdb626
Joost van der Griendt <joostvdg@gmail.com>
2018-08-21 23:18:08 +0200

    Add maven scripts

---
+++ vars/cleanMavenSettings.groovy
@@ -0,0 +1,13 @@
+def call() {
+    if (fileExists 'jenkins-settings.xml') {
+        if (isUnix()) {
+            // https://unix.stackexchange.com/questions/118217/chmod-silent-mode-how-force-exit-code-0-in-spite-of-error
+            sh 'rm jenkins-settings.xml || true'
+        } else {
+            // we assume windows
+            bat 'del "jenkins-settings.xml"'
+        }
+    } else {
+        echo '[INFO] jenkins-settings.xml does not exist, are you in the correct folder?'
+    }
+}
---
+++ vars/generateMavenSettings.groovy
@@ -0,0 +1,23 @@
+def call (String credentialsId) {
+    assert credentialsId: "CredentialsId must be given as parameter."
+
+    withCredentials([usernamePassword(credentialsId: 'github', passwordVariable: 'PSS', usernameVariable: 'USR')]) {
+        if ("${env.PSS}".startsWithAny('{') && "${env.PSS}".endsWithAny('}')) {
+            // we're safe
+            echo '[WARN] Encrypted maven password detected, can this agent decrypt it?'
+        } else {
+            echo '[WARN] Maven password not encrypted, please make sure to use the clean function (cleanMavenSettings)'
+        }
+        writeFile encoding: 'UTF-8', file: 'jenkins-settings.xml', text: '''<settings>
+            <servers>
+                <server>
+                    <id>artifactory</id>
+                    <username>${env.USR}</username>
+                    <password>${env.PSS}</password>
+                </server>
+            </servers>
+        </settings>'''
+    }
+
+
+}
