5dcfea0af8de4f49d0cf8a6b7591002441a0b451
Joost van der Griendt <joostvdg@gmail.com>
2018-08-23 17:25:45 +0200

    Maybe without the noncps method?

--- vars/mavenNextReleaseVersion.groovy
+++ vars/mavenNextReleaseVersion.groovy
@@ -1,15 +1,17 @@
 // Requires: https://wiki.jenkins.io/display/JENKINS/Pipeline+Utility+Steps+Plugin
 
 def call(String pomFile) {
-    def version = readVersionFromPom()
+    def pom = readMavenPom file: pomFile
+    String version = "${pom.version}"
+    version = version.replace("-SNAPSHOT", "")
     echo "[INFO] Maven pom version: ${version}"
     return version
 }
 
-@NonCPS
-def readVersionFromPom(String pomFile) {
-    def pom = readMavenPom file: pomFile
-    String version = "${pom.version}"
-    version = version.replace("-SNAPSHOT", "")
-    return version
-}
+//@NonCPS
+//def readVersionFromPom(String pomFile) {
+//    def pom = readMavenPom file: pomFile
+//    String version = "${pom.version}"
+//    version = version.replace("-SNAPSHOT", "")
+//    return version
+//}
