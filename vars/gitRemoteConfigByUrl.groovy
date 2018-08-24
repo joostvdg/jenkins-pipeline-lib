bb68754110616e06097086bc1efec055ca3d38b9
Joost van der Griendt <joostvdg@gmail.com>
2018-08-24 14:46:14 +0200

    Typo in assert

--- vars/gitRemoteConfigByUrl.groovy
+++ vars/gitRemoteConfigByUrl.groovy
@@ -1,6 +1,6 @@
 def call(String url, credentialsId) {
     assert url: 'Url is not set (first param)'
-    assert cerdentialsId: 'credentailsId is not set (second param)'
+    assert credentialsId: 'credentialsId is not set (second param)'
     assert url.contains('https://'): 'Url is not a https url, this only works with "https://....git" kind of urls'
 
     withCredentials([string(credentialsId: credentialsId, variable: 'TOKEN')]) {
