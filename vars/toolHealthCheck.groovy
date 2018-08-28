66ce5c956a27f0c33b20a4086e65020c3998ce85
Joost van der Griendt <joostvdg@gmail.com>
2018-08-28 22:45:43 +0200

    Add a bit of logging

--- vars/toolHealthCheck.groovy
+++ vars/toolHealthCheck.groovy
@@ -1,4 +1,5 @@
 def call (String url) {
     def responseCode = sh returnStdout: true, script: "curl -sL -w \"%{http_code}\" ${url} -o /dev/null --max-time 15"
+    echo "[INFO] reponse ${responseCode}"
     return responseCode
 }
