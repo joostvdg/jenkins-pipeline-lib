34d43d1641da9f79ab2d339e2d7d5b7c9a623429
Joost van der Griendt <joostvdg@gmail.com>
2018-08-28 22:40:58 +0200

    Use variable

--- vars/toolHealthCheck.groovy
+++ vars/toolHealthCheck.groovy
@@ -1,4 +1,4 @@
 def call (String url) {
-    def responseCode = sh returnStdout: true, script: 'curl -sL -w "%{http_code}" localhost:8080 -o /dev/null --max-time 15'
+    def responseCode = sh returnStdout: true, script: "curl -sL -w \"%{http_code}\" ${url} -o /dev/null --max-time 15"
     return responseCode
 }
