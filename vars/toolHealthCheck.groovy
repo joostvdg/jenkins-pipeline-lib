fe986d1cfb0570de6fbcf2b8ecbf6c7d15a8cbab
Joost van der Griendt <joostvdg@gmail.com>
2018-08-29 09:02:03 +0200

    Add error handling

--- vars/toolHealthCheck.groovy
+++ vars/toolHealthCheck.groovy
@@ -1,5 +1,12 @@
 def call (String url) {
-    def responseCode = sh returnStdout: true, script: "curl -sL -w \"%{http_code}\" ${url} -o /dev/null --max-time 15"
-    echo "[INFO] reponse ${responseCode}"
+    def responseCode = "000"
+    try {
+        responseCode = sh (returnStdout: true, script: "curl -sL -w \"%{http_code}\" ${url} -o /dev/null --max-time 15").trim()
+    } catch(err) {
+        echo "[WARN] caught ${err}"
+        responseCode = '520' // CloudFlare's 520 - Unknown Error: https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+    } finally {
+        echo "[INFO] reponse ${responseCode}"
+    }
     return responseCode
 }
