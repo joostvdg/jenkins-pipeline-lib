d85a7d53d7c66b5a8ee34d353ab1ad923c8adbf1
Joost van der Griendt <joostvdg@gmail.com>
2018-10-06 13:06:34 +0200

    Update to new plugin config

--- vars/anchoreScan.groovy
+++ vars/anchoreScan.groovy
@@ -1,10 +1,4 @@
 def call (imageLine) {
-    // def imageLine = 'appregister-backend:latest'
     writeFile file: 'anchore_images', text: imageLine
-    anchore name: 'anchore_images', bailOnFail: false, inputQueries: [
-            [query: 'cve-scan all'],
-            [query: 'list-packages all'],
-            [query: 'list-files all'],
-            [query: 'show-pkg-diffs base']
-    ]
+    anchore bailOnFail: false, bailOnPluginFail: false, name: 'anchore_images'
 }
