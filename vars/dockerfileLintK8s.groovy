13745c7c855b74f3411b973b1dd19bf0ed2c1f74
Joost van der Griendt <joostvdg@gmail.com>
2018-10-25 12:26:36 +0200

    parameterize filename with default

--- vars/dockerfileLintK8s.groovy
+++ vars/dockerfileLintK8s.groovy
@@ -1,5 +1,5 @@
-def call () {
-    def lintResult = sh returnStdout: true, script: 'hadolint Dockerfile'
+def call (def fileName = 'Dockerfile') {
+    def lintResult = sh returnStdout: true, script: "hadolint ${fileName}"
     if (lintResult.trim() == '') {
         println 'Lint finished with no errors'
     } else {
